fd22eeffd93f68febc3fa3e7d23fd47c14f9820e
undefinedzack <adhney.nawghare@gmail.com>
2020-08-30 12:09:47 +0530

    fixing index.html 404 error

--- polls/urls.py
+++ polls/urls.py
@@ -6,9 +6,9 @@ from . import views
 app_name = 'polls'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
-    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
-    path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('polls/<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('polls/<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('polls/<int:question_id>/vote/', views.vote, name='vote'),
 ]
 
 # urlpatterns = [
